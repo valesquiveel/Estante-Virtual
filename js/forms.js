f539a370ad378071504c9c8a71b1d916141d48a1
valesquiveel <valentinawpp25@gmail.com>
2023-12-11 22:43:24 -0300

    Finalizando:)

--- js/forms.js
+++ js/forms.js
@@ -24,6 +24,47 @@ cartao.on("click", function() {
         <input type="submit" value="Pagar">
     `;
     
-    $("#cDc").html(formulario);
-
+    $("#cDc").slideUp(1000, function() {
+        $(this).html(formulario).slideToggle();
+    });
+    
 });
+
+var pix = $("#pix")
+
+pix.on("click", function(){
+    var formulario2 = `<input type="button" value="GERAR CÓDIGO QR" id="gerar-codigo">`
+
+    $("#cDc").slideUp(1000, function() {
+        $(this).html(formulario2).slideToggle();
+
+        $("#gerar-codigo").on("click", function(){
+            var codigoQR = `<img src="../images/Fim Compras/QR.png">`
+            $("#cDc").html(codigoQR);
+        })
+    });
+
+    
+});
+
+var boleto = $("#boleto")
+
+boleto.on("click", function(){
+    var formulario3 = `<label for="nome_completo">Nome Completo:</label>
+    <input type="text" id="nome_completo" name="nome_completo" required><br><br>
+
+    <label for="cpf">CPF:</label>
+    <input type="text" id="cpf" name="cpf" required><br><br>
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required><br><br>
+
+    <label for="endereco">Endereço:</label>
+    <input type="text" id="endereco" name="endereco" required><br><br>
+
+    <input type="submit" value="Gerar Boleto">`
+
+    $("#cDc").slideUp(1000, function() {
+        $(this).html(formulario3).slideToggle();
+    });
+})
